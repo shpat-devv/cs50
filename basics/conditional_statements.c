d7bbc1686c2341f8d6a5ae836c0cc7f2c4e1ef9a
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-05-23 09:38:45 +0200

    conditional statements

---
+++ basics/conditional_statements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num = 20;
+
+    if (num > 50){
+        printf("num is bigger than 50");
+    }else{
+        printf("num is smaller than 50");
+    }
+
+    return 0;
+}
