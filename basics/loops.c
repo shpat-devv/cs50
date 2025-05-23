7977a059145703302e6928293a20612330fa6b91
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-05-23 10:24:42 +0200

    loops

---
+++ basics/loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    //for loops:
+    for(int i = 0; i < 10; i++){
+        printf("hi");
+    }
+    //while loops:
+    int i = 10;
+    while(i < 100){
+        i += 10;
+    }
+
+    //do while:
+    do{
+        int i = 10;
+    }while(i < 100);{
+        i += 10;
+    }
+
+    return 0;
+}
