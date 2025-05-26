b15965f501417970f9ee71ff76c0ca169f380c3a
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-05-26 10:01:10 +0200

    started readability assigment

---
+++ exams/readability/readability.c
@@ -0,0 +1,14 @@
+/*
+This is a program to determine the grade level of books, it scans over the text and calculates these factos:
+
+average amount of letters per word
+average aamount of letters per sentence
+
+The formula to calculate the grade:
+
+index = 0.0588 * L - 0.296 * S - 15.8
+
+where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
+*/
+
+
